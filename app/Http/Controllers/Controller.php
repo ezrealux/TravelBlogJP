20fe49aa739d49dc428d8cbed08bc91dc9cae135
ezralux <wilsonliao1226@gmail.com>
2025-09-10 17:32:32 +0800

    swagger api doc success

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="My TravelBlog API",
+ *     version="1.0.0",
+ *     description="API documentation for the TravelBlogJP project"
+ * )
+ */
+
 abstract class Controller
 {
     //
